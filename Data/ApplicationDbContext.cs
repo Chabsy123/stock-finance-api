32f8d80ddd19bdfb6585afed400bd4f10f749636
Chukwu Sydney <chukwusydney061@gmail.com>
2026-02-04 14:17:59 +0100

    Update Models And EF Core Migrations

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using stock_finance_api.Models;
+
+namespace stock_finance_api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) 
+        {
+            
+        }
+
+        public DbSet<Stock> Stock { get; set;  }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
---
+++ Models/Stock.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace stock_finance_api.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+
+        public string MyProperty { get; set; } = string.Empty;
+
+        public string CompanyName { get; set; } = string.Empty;
+        //eighteen digits, two decimal places
+        [Column(TypeName = "decimal(18,2)")]
+
+        public decimal Purchase { get; set; }
+
+        public decimal Dividend { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+
+        public decimal LastDiv { get; set; }
+
+        public string Industry { get; set; } = string.Empty;
+
+        public long MarketCap { get; set; }
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+    }
+}
