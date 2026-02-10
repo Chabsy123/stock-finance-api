a60928aa88f58dcbd3c5cd4b6d49a5026f2682f6
Chukwu Sydney <chukwusydney061@gmail.com>
2026-02-10 09:20:50 +0100

    Implement Portfolio GET, CREATE $ DELETE Operations

---
+++ Interface/IPortfolioRepository.cs
@@ -0,0 +1,12 @@
+﻿using stock_finance_api.Models;
+
+namespace stock_finance_api.Interface
+{
+    public interface IPortfolioRepository
+    {
+        Task<List<Stock>> GetUserPortfolio(AppUser user);
+        Task<Portfolio> CreateAsync(Portfolio portfolio);
+
+        Task<Portfolio> DeletePortfolio(AppUser appUser, string symbol);
+    }
+}
